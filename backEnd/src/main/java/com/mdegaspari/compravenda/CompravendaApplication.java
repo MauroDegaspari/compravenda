9710a8c29d96e35784524286302a4a8a710fd9b5
MauroDegaspari <mauro_degaspari@hotmail.com>
2022-11-14 22:12:18 -0300

    Subindo projeto inicial

---
+++ backEnd/src/main/java/com/mdegaspari/compravenda/CompravendaApplication.java
@@ -0,0 +1,13 @@
+package com.mdegaspari.compravenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompravendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompravendaApplication.class, args);
+	}
+
+}
---
+++ backEnd/src/test/java/com/mdegaspari/compravenda/CompravendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdegaspari.compravenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompravendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
