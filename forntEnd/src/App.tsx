e8e9e25d16b9cda7b16c75626b16c556e901ba71
MauroDegaspari <mauro_degaspari@hotmail.com>
2022-11-19 10:36:30 -0300

    frontEnd - componete BotaoSmS

--- forntEnd/src/App.tsx
+++ forntEnd/src/App.tsx
@@ -1,6 +1,11 @@
+import BotaoSmS from "./components/BotaoSmS"
+
 function App() {
   return(
+    <> 
     <h1>E ai porra...</h1>
+    <BotaoSmS />
+    </>
   )
 }
 
---
+++ forntEnd/src/components/BotaoSmS/index.tsx
@@ -0,0 +1,12 @@
+import  icone from "../../assets/img/SMS.svg";
+import './styles.css';
+
+function BotaoSmS() {
+    return (
+        <div className="quadrado-icone-btn">
+            <img src={icone}></img>
+        </div>
+    )
+}
+
+export default BotaoSmS;
